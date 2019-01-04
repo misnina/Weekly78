7601c483779b888cd28e5714cd6594bc6d731186
Nina <ninarachaebuie@gmail.com>
2019-01-04 14:43:32 -0600

    Click on shape to change shape

---
+++ Weekly78/Assets/scripts/ShapeChanger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeChanger : MonoBehaviour
+{
+    public Sprite square;
+    public Sprite triangle;
+    public Sprite circle;
+    public Sprite diamond;
+
+    private SpriteRenderer sp;
+
+    private bool isSquare = true;
+    private bool isTriangle;
+    private bool isCircle;
+    private bool isDiamond;
+
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        if (isSquare)
+        {
+            sp.sprite = triangle;
+            isTriangle = true;
+            isSquare = false;
+        }
+        else if (isTriangle)
+        {
+            sp.sprite = circle;
+            isCircle = true;
+            isTriangle = false;
+        }
+        else if (isCircle)
+        {
+            sp.sprite = diamond;
+            isDiamond = true;
+            isCircle = false;
+        }
+        else if (isDiamond)
+        {
+            sp.sprite = square;
+            isSquare = true;
+            isDiamond = false;
+        }
+    }
+}
