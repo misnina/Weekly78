123a512b7ac59178ee165e9a9e26926f37e30f0f
Nina <ninarachaebuie@gmail.com>
2019-01-04 15:46:57 -0600

    Basic gameplay, detection of shapes

---
+++ Weekly78/Assets/scripts/RowController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowController : MonoBehaviour
+{
+
+    public ShapeChanger[] shapes = new ShapeChanger[5];
+    public int[] keys = new int [5];
+    public bool[] positions = new bool[5];
+    private int keyCount;
+
+    
+    private void Start()
+    {
+        shapes[0] = (this.transform.Find("ShowHide").gameObject.transform.Find("Shape0").GetComponent<ShapeChanger>());
+        shapes[1] = (this.transform.Find("ShowHide").gameObject.transform.Find("Shape1").GetComponent<ShapeChanger>());
+        shapes[2] = (this.transform.Find("ShowHide").gameObject.transform.Find("Shape2").GetComponent<ShapeChanger>());
+        shapes[3] = (this.transform.Find("ShowHide").gameObject.transform.Find("Shape3").GetComponent<ShapeChanger>());
+        shapes[4] = (this.transform.Find("ShowHide").gameObject.transform.Find("Shape4").GetComponent<ShapeChanger>());
+        shapes[5] = (this.transform.Find("ShowHide").gameObject.transform.Find("Shape5").GetComponent<ShapeChanger>());
+    }
+
+    private void Update()
+    {
+        foreach (ShapeChanger shape in shapes)
+        {
+            keys[keyCount] = shape.key;
+            if (keyCount == 4)
+            {
+                keyCount = 0;
+            }
+            else
+            {
+                keyCount++;
+            }
+              
+           
+        }
+    }
+
+
+}
--- Weekly78/Assets/scripts/ShapeChanger.cs
+++ Weekly78/Assets/scripts/ShapeChanger.cs
@@ -16,6 +16,8 @@ public class ShapeChanger : MonoBehaviour
     private bool isCircle;
     private bool isDiamond;
 
+    public int key;
+
     void Start()
     {
         sp = GetComponent<SpriteRenderer>();
@@ -24,7 +26,23 @@ public class ShapeChanger : MonoBehaviour
     
     void Update()
     {
-        
+        if (isSquare)
+        {
+            key = 0;
+        }
+        else if (isTriangle)
+        {
+            key = 1;
+        }
+        else if (isCircle)
+        {
+            key = 2;
+        }
+        else if (isDiamond)
+        {
+            key = 3;
+        }
+
     }
 
     private void OnMouseUp()
@@ -34,24 +52,28 @@ public class ShapeChanger : MonoBehaviour
             sp.sprite = triangle;
             isTriangle = true;
             isSquare = false;
+            key = 0;
         }
         else if (isTriangle)
         {
             sp.sprite = circle;
             isCircle = true;
             isTriangle = false;
+            key = 1;
         }
         else if (isCircle)
         {
             sp.sprite = diamond;
             isDiamond = true;
             isCircle = false;
+            key = 2;
         }
         else if (isDiamond)
         {
             sp.sprite = square;
             isSquare = true;
             isDiamond = false;
+            key = 3;
         }
     }
 }
