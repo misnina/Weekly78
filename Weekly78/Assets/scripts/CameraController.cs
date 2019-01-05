6aca5306f88a30bea5660e21560faa04627c2f47
Nina <ninarachaebuie@gmail.com>
2019-01-05 11:36:40 -0600

    Add basic player movement and camera follow

---
+++ Weekly78/Assets/scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+   
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
---
+++ Weekly78/Assets/scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 5;
+    public bool isGrounded;
+    public int jumpPower = 250;
+
+    private float moveX;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sp;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+
+
+        //ANIMATIONS
+
+        //FLIP
+        if (moveX < 0.0f)
+        {
+            sp.flipX = true;
+        } 
+        else if(moveX > 0.0f)
+        {
+            sp.flipX = false;
+        }
+
+        //PHYSICS
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        //JUMPING CODE
+        rb.AddForce(Vector2.up * jumpPower);
+    }
+
+}
