03c89c28906df7408a67cf5d6a1c9187b0acce96
Nina <ninarachaebuie@gmail.com>
2019-01-07 20:09:15 -0600

    Add Exit Door

---
+++ Weekly78/Assets/scripts/AutoScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroller : MonoBehaviour
+{
+
+    public Transform endPoint;
+    public Transform player;
+    public float speed;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(endPoint.position.x, player.position.y, transform.position.z), step);
+    }
+}
---
+++ Weekly78/Assets/scripts/ExitDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitDoor : MonoBehaviour
+{
+    public string sceneToLoad;
+    private bool playerTrigger;
+
+
+    private void Update()
+    {
+        if(playerTrigger && Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerTrigger = true;
+        }
+    }
+}
