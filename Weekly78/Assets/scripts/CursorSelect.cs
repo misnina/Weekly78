3d819868b3ab435b2bebde1b13af6eda6174498b
Nina <ninarachaebuie@gmail.com>
2019-01-09 16:12:21 -0600

    Add Pixel Font Package and Create UI Start Screen

---
+++ Weekly78/Assets/scripts/CursorSelect.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorSelect : MonoBehaviour
+{
+    private Vector2 cursorPos;
+    private bool triggeredOnce;
+    public Transform startPos;
+    public Transform endPos;
+
+    private bool playTrigger;
+    private bool controlsTrigger;
+    private bool creditsTrigger;
+    private bool backTrigger;
+
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(playTrigger)
+            {
+                SceneManager.LoadScene("test");
+                //Replace for level 1
+            }
+            else if (controlsTrigger)
+            {
+                UIManager.instance.ShowWindow(1);
+            } else if (creditsTrigger)
+            {
+                UIManager.instance.ShowWindow(2);
+
+            } else if (backTrigger)
+            {
+                UIManager.instance.ShowWindow(0);
+            }
+        }
+
+        if (!triggeredOnce)
+        {
+            if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                transform.Translate(new Vector2(0f, 1f));
+                triggeredOnce = true;
+                Invoke("ResetTrigger", 0.25f);
+            }
+            else if (Input.GetAxisRaw("Vertical") == -1)
+            {
+                transform.Translate(new Vector2(0f, -1f));
+                triggeredOnce = true;
+                Invoke("ResetTrigger", 0.25f);
+            }
+        }
+    }
+
+    private void ResetTrigger()
+    {
+        triggeredOnce = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "BoundsDown")
+        {
+            Debug.Log("Collided");
+            transform.position = startPos.transform.position;
+        }
+        if (other.gameObject.name == "BoundsUp")
+        {
+            transform.position = endPos.transform.position;
+        }
+
+        //Buttons
+        if(other.gameObject.name == "Play")
+        {
+            playTrigger = true;
+        }
+        if(other.gameObject.name == "Controls")
+        {
+            controlsTrigger = true;
+        }
+        if(other.gameObject.name == "Credits")
+        {
+            creditsTrigger = true;
+        }
+
+        if (other.gameObject.name == "Back")
+        {
+            backTrigger = true;
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Play")
+        {
+            playTrigger = false;
+        }
+
+        if (other.gameObject.name == "Controls")
+        {
+            controlsTrigger = false;
+        }
+
+        if (other.gameObject.name == "Credits")
+        {
+            creditsTrigger = false;
+        }
+
+        if (other.gameObject.name == "Back")
+        {
+            backTrigger = false;
+        }
+    }
+
+
+}
---
+++ Weekly78/Assets/scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject[] windows;
+    public static UIManager instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public void ShowWindow(int windowNumber)
+    {
+        for(int i = 0; i < windows.Length; i++)
+        {
+            if (windowNumber == i)
+            {
+                windows[i].SetActive(true);
+            } else
+            {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+}
