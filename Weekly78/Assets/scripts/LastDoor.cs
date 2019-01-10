114bb272846be68f80c923cd29f032d8b487f6f2
Nina <ninarachaebuie@gmail.com>
2019-01-10 06:18:41 -0600

    Complete level 01

---
+++ Weekly78/Assets/scripts/LastDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastDoor : MonoBehaviour
+{
+
+    private bool playerTrigger;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (playerTrigger)
+            {
+                Destroy(BackgroundMusic.instance.gameObject);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerTrigger = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerTrigger = false;
+        }
+
+    }
+}
